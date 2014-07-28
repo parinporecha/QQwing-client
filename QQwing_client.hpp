fddf08b555df4e97c49a511e404ba1232089f967
Parin Porecha <parinporecha@gmail.com>
2014-07-28 21:55:43 +0200

    Initialize repo with wrapper code, header, Vala client, and vapi

---
+++ QQwing_client.hpp
@@ -0,0 +1,10 @@
+#include <glib.h>
+
+G_BEGIN_DECLS
+
+using namespace qqwing;
+using namespace std;
+
+void printSimplePuzzle ();
+
+G_END_DECLS
---
+++ QQwing_client.vapi
@@ -0,0 +1,2 @@
+[CCode (cheader_filename = "QQwing_client.hpp", lower_case_cprefix = "")]
+void printSimplePuzzle ();
